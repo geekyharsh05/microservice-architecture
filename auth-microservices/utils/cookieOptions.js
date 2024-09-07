e8f44931727259fc2039fbbc6951d09f8c8e1074
geekyharsh05 <hp424805@gmail.com>
2024-09-07 19:21:39 +0530

    chore: Refactor cookieOptions.js to improve code structure and readability

--- auth-microservices/utils/cookieOptions.js
+++ auth-microservices/utils/cookieOptions.js
@@ -1,6 +1,10 @@
-const options = {
+export const logoutOptions = {
   httpOnly: true,
   secure: true,
+};
+
+const options = {
+  ...logoutOptions,
   sameSite: "strict",
   maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
 };
