61078a091779f8607f2fc2a9f90451fb791975bc
munna-yadav <mail.munna50@gmail.com>
2024-09-19 23:02:29 +0530

    chore: Update comments route

--- post-microservices/routes/index.js
+++ post-microservices/routes/index.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
-
+import CommentRoutes from "../routes/comment.routes.js"
+import PostRoutes from "../routes/post.routes.js"
 const router = Router();
 
 router.use("/post", PostRoutes);
+router.use("/post/comment",CommentRoutes)
 
 export default router;
