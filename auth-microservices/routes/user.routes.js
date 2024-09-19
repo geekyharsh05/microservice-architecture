9cd594796e68b279237f8920556a88fbeb1a2331
munna-yadav <mail.munna50@gmail.com>
2024-09-19 20:01:21 +0530

    update: update user route for UpdateUser

--- auth-microservices/routes/user.routes.js
+++ auth-microservices/routes/user.routes.js
@@ -6,5 +6,6 @@ const router = Router();
 
 router.get("/getUser/:id", isAuthenticated, UserController.getUserById);
 router.get("/getUsers", isAuthenticated, UserController.getUsers);
+router.put("/updateUser/:id", isAuthenticated, UserController.changeUserDetail);
 
 export default router;
