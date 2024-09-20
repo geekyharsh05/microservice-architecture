cff2d82b4b4c99482b423396af35fabf0fe14e81
geekyharsh05 <hp424805@gmail.com>
2024-09-20 14:59:40 +0530

    chore: Update CORS configuration in index.js

--- auth-microservices/index.js
+++ auth-microservices/index.js
@@ -13,7 +13,11 @@ const PORT = process.env.PORT ?? 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.POST_MICRO_URL,
+  })
+);
 app.use(morgan("dev"));
 
 app.use("/api/v1", Routes);
